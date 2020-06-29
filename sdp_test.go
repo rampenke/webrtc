824e39558900e6f7b7a24eec9ce8b2bef0d8991a
Woodrow Douglass <wdouglass@carnegierobotics.com>
2020-06-29 11:55:13 -0400

    Add a test for SDP Media Descriptions
    This tests that a fingerprint gets included.

--- sdp_test.go
+++ sdp_test.go
@@ -3,7 +3,12 @@
 package webrtc
 
 import (
+	"math/big"
+	"math/rand"
 	"testing"
+
+	"crypto/rsa"
+	"crypto/x509"
 
 	"github.com/pion/sdp/v2"
 	"github.com/stretchr/testify/assert"
@@ -289,4 +294,79 @@ func TestHaveApplicationMediaSection(t *testing.T) {
 
 		assert.True(t, haveApplicationMediaSection(s))
 	})
+}
+
+func TestMediaDescriptionFingerprints(t *testing.T) {
+
+	engine := &MediaEngine{}
+	engine.RegisterCodec(NewRTPH264Codec(DefaultPayloadTypeH264, 90000))
+	engine.RegisterCodec(NewRTPOpusCodec(DefaultPayloadTypeOpus, 48000))
+
+	priv := &rsa.PrivateKey{
+		PublicKey: rsa.PublicKey{
+			N: big.NewInt(0),
+			E: 65537},
+		D: big.NewInt(0),
+		Primes: []*big.Int{
+			big.NewInt(0),
+			big.NewInt(0)}}
+
+	priv.PublicKey.N.SetString("28797351613986386053553799925930523856556567114192281047262209832157609093808726167332754674311193610314657875506491138278042731907378585474871313858539972620506584990256230463055263267832879992278549860679656698948688583000439632452783407023880239480944921280007744201325920812272533869383647979379571567184966151661498617440635276895590409657305405652079399213839457000740924007510776041152563237619320062950310310438147558795039302932064921857560398083339872243152893560088753009260598409134324107854684242358180483795713733484804681247651783660486476023730134351951739801106571526219927868405908703470223627112807", 10)
+	priv.D.SetString("13831182946327257485202449917886310014691411920799402330358005079372340226830908854069199366514998264673001296173426027428444253967202583807506055301970836598801986293823265732114538686262348955810487418780437827682192815159449445514995549123253708563738091263420720573494911415046501191793781946792358121675854595191396101396382587442882935489858060385461971664771858075315050803868636800573416034338516755974135293658834448940549284239795621310918325574568182905526946604653505127579763211271140295994151861675935967787208484255187831364945358888205056068887846318281877223185543644778354964586968082185382499560369", 10)
+	priv.Primes[0].SetString("178543263971563445611846753647960454328877138110494552805342946372410027118203982039511174780961824855984433336212442651027464024789151422760784357833919112679849875175290866326856420160636005838393018472572662993504365089058201870664786291964784723445519174387159504797796942043782152797738931277870772462409", 10)
+	priv.Primes[1].SetString("161290608076779279684432358029784616172585842112589351628730355733876482119583845522812629523767500848816306923105072613282149253767315567637739875378081888629173569299624561645978174853005648841950258779154997443239884879774661727133962280797487609122726047048985176827535144336292828924682552883542838648623", 10)
+
+	priv.Precompute()
+
+	testsdp := "v=0\r\no= 0 0   \r\ns=\r\na=group:BUNDLE video audio\r\nm=video 9 UDP/TLS/RTP/SAVPF 102\r\nc=IN IP4 0.0.0.0\r\na=setup:active\r\na=mid:video\r\na=ice-ufrag\r\na=ice-pwd\r\na=rtcp-mux\r\na=rtcp-rsize\r\na=rtpmap:102 H264/90000\r\na=fmtp:102 level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42001f\r\na=sendonly\r\na=fingerprint:sha-256 34:1F:E5:AD:AC:79:55:8E:80:38:A5:57:EF:75:34:EA:F5:22:A4:DB:E2:5D:82:67:C5:15:A3:FA:B5:E9:B8:0B\r\nm=audio 9 UDP/TLS/RTP/SAVPF 111\r\nc=IN IP4 0.0.0.0\r\na=setup:active\r\na=mid:audio\r\na=ice-ufrag\r\na=ice-pwd\r\na=rtcp-mux\r\na=rtcp-rsize\r\na=rtpmap:111 opus/48000/2\r\na=fmtp:111 minptime=10;useinbandfec=1\r\na=sendonly\r\na=fingerprint:sha-256 34:1F:E5:AD:AC:79:55:8E:80:38:A5:57:EF:75:34:EA:F5:22:A4:DB:E2:5D:82:67:C5:15:A3:FA:B5:E9:B8:0B\r\n"
+
+	template := &x509.Certificate{
+		SerialNumber: big.NewInt(2020),
+	}
+
+	r := rand.New(rand.NewSource(2020))
+	certBytes, err := x509.CreateCertificate(r, template, template, &priv.PublicKey, priv)
+	assert.Equal(t, nil, err)
+
+	cert, err := x509.ParseCertificate(certBytes)
+
+	assert.Equal(t, nil, err)
+
+	s := &sdp.SessionDescription{}
+	media := []mediaSection{
+		{
+			id: "video",
+			transceivers: []*RTPTransceiver{{
+				kind: RTPCodecTypeVideo,
+			}},
+			data: false,
+		},
+		{
+			id: "audio",
+			transceivers: []*RTPTransceiver{{
+				kind: RTPCodecTypeAudio,
+			}},
+			data: false,
+		},
+	}
+
+	for i := 0; i < 2; i++ {
+		media[i].transceivers[0].setSender(&RTPSender{})
+		media[i].transceivers[0].setDirection(RTPTransceiverDirectionSendonly)
+	}
+
+	s, err = populateSDP(s, false,
+		&Certificate{
+			privateKey: priv,
+			x509Cert:   cert,
+		},
+		false, engine, sdp.ConnectionRoleActive, []ICECandidate{}, ICEParameters{}, media, ICEGatheringStateNew)
+
+	assert.Equal(t, nil, err)
+
+	sdparray, err := s.Marshal()
+
+	assert.Equal(t, nil, err)
+
+	assert.Equal(t, testsdp, string(sdparray))
 }
